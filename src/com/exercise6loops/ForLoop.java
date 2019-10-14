407da21a3e95ed3bc1131d67c98d379de5458109
DanielCandiaWR <padilla_da@hotmail.com>
2019-10-14 13:06:59 -0500

    loops examples with for

--- src/com/exercise6loops/ForLoop.java
+++ src/com/exercise6loops/ForLoop.java
@@ -2,6 +2,39 @@ package com.exercise6loops;
 
 public class ForLoop {
 	public static void main(String[] args) {
+		//Constant declaration
 		
+		//Variable declaration
+		
+		//Object construction
+		
+		System.out.println("Create a series of 1-100 1 by 1");
+		for(int i=1;i<=100;i++) {
+			System.out.print(i+" ");
+		}
+		System.out.println();
+		System.out.println("Create a series of 100-1 1 by 1");
+		for(int i=100;i>=1;i--) {
+			System.out.print(i+" ");
+		}
+		System.out.println();
+		System.out.println("Create a series of 1-100 with increments of 2");
+		for(int i=1;i<=100;i+=2) {
+			System.out.print(i+" ");
+		}
+		System.out.println("\nDisplay odd numbers from 100 to 200");
+		for(int i=100;i<200;i++) {
+			if(i%2==0) {
+				continue;
+			}
+			System.out.print(i+" ");
+		}
+		System.out.println("\nDisplay just the half of the sequence 100 to 200");
+		for(int i=100;i<200;i++) {
+			System.out.print(i+" ");
+			if(i==150) {
+				break;
+			}
+		}
 	}
 }
